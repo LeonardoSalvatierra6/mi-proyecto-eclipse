67f2b82d10fa8caa61fb1ff6f404666d1cf506b2
Leonardo Salvatierra <leonardoandres98@gmail.com>
2025-08-15 13:56:48 +0200

    TrenCarga.java

--- LeonardoSalvatierraExamenOrdinaria/EjercicioPrimeroExmaen/src/ejerciciouno/TrenCarga.java
+++ LeonardoSalvatierraExamenOrdinaria/EjercicioPrimeroExmaen/src/ejerciciouno/TrenCarga.java
@@ -2,13 +2,12 @@ package ejerciciouno;
 
 public class TrenCarga extends Tren {
 
-	//Clase para TrenCarga. No pongo la variable int pasajerosPorVagon porque en un tren de carga no hay pasajeros
+
 	public TrenCarga(String id, String nombre, double velocidadMax, int numVagones) throws TrenInvalidoException {
 		super(id, nombre, velocidadMax);
 		
 	}
 
-	//Sobreescribo otra vez mostrarInformacion para que vaya con la clase TrenCarga
 	@Override
 	public void mostrarInformacion() {
 
@@ -16,3 +15,4 @@ public class TrenCarga extends Tren {
 	}
 
 }
+
