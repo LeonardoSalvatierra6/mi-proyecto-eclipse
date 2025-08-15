2f1ca6654af6a9eda932046386a1bcdb759743de
Leonardo Salvatierra <leonardoandres98@gmail.com>
2025-08-15 13:53:56 +0200

    Add files via upload

---
+++ Movil.java
@@ -0,0 +1,39 @@
+package ejercicio2;
+
+import java.util.Scanner;
+
+public class Movil {
+
+	protected String movil;
+	public static void main(String[] args)  {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Introduce tu numero movil");
+		String movil = scanner.nextLine();
+
+//Manejo de excepciones
+		
+		try {
+			ValidarMovil(movil);
+			System.out.println("Movil válido.");
+		} 
+		catch (MovilInvalidException e) {
+			System.out.println("Error: " +e.getMessage());
+			System.out.println("Por favor, inteta nuevamente. \n");
+		}
+		scanner.close();
+	}
+
+//Método de validación
+	
+	public static void ValidarMovil(String movil)
+	throws MovilInvalidException{
+		if (movil == null || !movil.matches("\\d{8}"));
+		{
+			throw new MovilInvalidException("Movil inválido. Debe tener 8 dígitos.");
+			
+		}
+	}
+}
+
+
---
+++ MovilInvalidException.java
@@ -0,0 +1,10 @@
+package ejercicio2;
+
+public class MovilInvalidException extends Exception {
+
+	public MovilInvalidException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
