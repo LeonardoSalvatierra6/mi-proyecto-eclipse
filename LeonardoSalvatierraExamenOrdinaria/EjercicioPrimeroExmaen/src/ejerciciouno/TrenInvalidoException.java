be4eea0d5a8675bd6f7fc8788f4ead13b7596d5f
Leonardo Salvatierra <leonardoandres98@gmail.com>
2025-08-15 13:57:13 +0200

    TrenInvalidoException.java

--- LeonardoSalvatierraExamenOrdinaria/EjercicioPrimeroExmaen/src/ejerciciouno/TrenInvalidoException.java
+++ LeonardoSalvatierraExamenOrdinaria/EjercicioPrimeroExmaen/src/ejerciciouno/TrenInvalidoException.java
@@ -1,10 +1,10 @@
 package ejerciciouno;
 
 
-//clase exception para lanzar mensaje
 public class TrenInvalidoException extends Exception {
 
 	public TrenInvalidoException(String mensaje) {
 		super(mensaje);
 	}
 }
+
